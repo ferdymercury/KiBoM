eb43a5f3e36f581f42c645c8cf0d8498dac7d315
Salvador E. Tropea <salvador@inti.gob.ar>
2020-07-18 13:47:15 -0300

    Fixed another chunk of code repeated during merge.

--- kibom/html_writer.py
+++ kibom/html_writer.py
@@ -135,10 +135,6 @@ def WriteHTML(filename, groups, net, headings, head_names, prefs):  # (#120)
                 if link_datasheet and headings[n] == link_datasheet:
                     r = '<a href="' + group.getField(ColumnList.COL_DATASHEET) + '">' + r + '</a>'
 
-                # Link this column to Digi-Key? (#114)
-                if link_digikey and headings[n] in link_digikey:
-                    r = '<a href="http://search.digikey.com/scripts/DkSearch/dksus.dll?Detail&name=' + r + '">' + r + '</a>'
-
                 if (len(r) == 0) or (r.strip() == "~"):
                     bg = BG_EMPTY
                 else:
